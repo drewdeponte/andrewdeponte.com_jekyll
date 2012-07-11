3f01c6f7bd179986afff3e55072432a4d6a2b304
Andrew De Ponte <cyphactor@gmail.com>
2012-07-11 08:35:40 -0700

    Added a plugin to handle archive page generation.
    I also added a liquid tag to handle the generation of the archive page
    listing including the links so that it is all self contained. The only
    thing left is to throw some style on the archives list.

---
+++ _plugins/archive_pages.rb
@@ -0,0 +1,56 @@
+module Jekyll
+  class ArchivePage < Page
+    def initialize(site, base, dir, period, posts)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'archive_index.html')
+
+      self.data['period'] = period
+      self.data['period_posts'] = posts
+      archive_title_prefix = site.config['archive_title_prefix'] || 'Archive: &ldquo;'
+      archive_title_suffix = site.config['archive_title_suffix'] || '&rdquo;'
+      self.data['title'] = "#{archive_title_prefix}#{period["month"]}-#{period["year"]}#{archive_title_suffix}"
+    end
+  end
+  
+  class ArchivePageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'archive_index'
+        site.posts.group_by{ |c| {"month" => c.date.month, "year" => c.date.year} }.each do |period, posts|
+          archive_dir = File.join(period["year"].to_s(), "%02d" % period["month"].to_s())
+          site.pages << ArchivePage.new(site, site.source, archive_dir, period, posts)
+        end
+      end
+    end
+  end
+
+  class ArchivesListTag < Liquid::Tag
+    safe = true
+
+    def initialize(tag_name, text, tokens)
+      super
+    end
+
+    def render(context)
+      archives = []
+      context.registers[:site].posts.group_by { |c| { "month" => c.date.month, "year" => c.date.year } }.each do |period, posts|
+        archives << { :year => period['year'].to_s, :month => '%02d' % period['month'].to_s, :count => posts.size }
+      end
+
+      html = "<div class=\"archives\"><h2>Archives</h2>"
+      archives.reverse.each do |archive_info|
+        html << "<a href=\"/#{archive_info[:year]}/#{archive_info[:month]}\" class=\"archive\"><span class=\"archive_period\">#{archive_info[:year]} - #{archive_info[:month]}</span><span class=\"archive_count\">(#{archive_info[:count]})</span></a>"
+      end
+      html << "</div>"
+      return html
+    end
+  end
+end
+
+Liquid::Template.register_tag('archives_list', Jekyll::ArchivesListTag)
